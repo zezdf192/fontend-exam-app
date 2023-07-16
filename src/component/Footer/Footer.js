5aa90a7f25efa514c0ad0e359f33a4708f24b53a
ducduy192 <duy192203@gmail.com>
2023-07-16 10:40:38 +0700

    edit language footer

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,14 +1,17 @@
+import { useTranslation } from 'react-i18next'
+
 import './Footer.scss'
 import facebook from '../../styles/icon/facebook.png'
 import youtube from '../../styles/icon/youtube.png'
 
 function FooterContent() {
+    const { t } = useTranslation()
     return (
         <div className="footer-container">
             <div className="footer-content row">
                 <div className="col-lg-4 col-sm-12 footer-col">
                     <h3 className="title-footer">Exam App</h3>
-                    <span className="text-footer">Neu ban muon biet them ve Exam App hay theo doi qua</span>
+                    <span className="text-footer">{t('footer.about')}</span>
                     <div className="social-footer">
                         <a target="_blank" href="https://www.facebook.com/duynguyen1923">
                             <img src={facebook} alt="" />
@@ -19,12 +22,12 @@ function FooterContent() {
                     </div>
                 </div>
                 <div className="col-lg-4 col-sm-12 footer-col">
-                    <h3 className="title-footer">Lien he</h3>
+                    <h3 className="title-footer">{t('footer.contact')}</h3>
                     <span className="text-footer">{`(+84) 395 573 465`}</span>
                     <span className="text">ducduyzajzero@gmail.com</span>
                 </div>
                 <div className="col-lg-4 col-sm-12 footer-col">
-                    <h3 className="title-footer">Ban quyen thuoc Exam App</h3>
+                    <h3 className="title-footer">{t('footer.reserved')}</h3>
                 </div>
             </div>
         </div>
